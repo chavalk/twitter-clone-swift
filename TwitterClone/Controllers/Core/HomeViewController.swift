f47e5a8c3a4c9848e1e688183d477a51ce7e7f50
chavalk <chavalk@hotmail.com>
2022-12-11 01:51:47 -0600

    Add table view in home view controller.

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let timelineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(timelineTableView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        timelineTableView.frame = view.frame
     }
-    */
-
 }
