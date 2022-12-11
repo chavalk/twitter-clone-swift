e871fb4f17a25c4ab0c7eb99c2e8172a6dce5e09
chavalk <chavalk@hotmail.com>
2022-12-11 01:58:24 -0600

    Add delegate and data source protocols with extension.

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -11,17 +11,31 @@ class HomeViewController: UIViewController {
 
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(timelineTableView)
+        timelineTableView.delegate = self
+        timelineTableView.dataSource = self
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         timelineTableView.frame = view.frame
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Tweet"
+        return cell
     }
 }
