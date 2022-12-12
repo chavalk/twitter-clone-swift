9b3098577122086633f6c40461b5ffeba8a7ac12
chavalk <chavalk@hotmail.com>
2022-12-11 23:31:17 -0600

    Add avatar image view and its contraints.

---
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 12/11/22.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
