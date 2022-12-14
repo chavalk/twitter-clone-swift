74f67773cd53ce46e0dc4fe15d4951d1792fd572
chavalk <chavalk@hotmail.com>
2022-12-14 16:21:04 -0600

    Add table view to profile view controller.

--- TwitterClone/Controllers/General/ProfileViewController.swift
+++ TwitterClone/Controllers/General/ProfileViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private let profileTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.title = "Profile"
+        view.addSubview(profileTableView)
     }
 }
