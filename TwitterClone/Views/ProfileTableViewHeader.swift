439e95a56fa2d922b074418b3aafd900259d7a5e
chavalk <chavalk@hotmail.com>
2022-12-14 17:23:14 -0600

    Add profile header image view to profile table view header.

--- TwitterClone/Views/ProfileTableViewHeader.swift
+++ TwitterClone/Views/ProfileTableViewHeader.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class ProfileTableViewHeader: UIView {
 
+    private let profileHeaderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "header")
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .red
+        addSubview(profileHeaderImageView)
+        
+        configureConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    private func configureConstraints() {
+        let profileHeaderImageViewConstraints = [
+            profileHeaderImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            profileHeaderImageView.topAnchor.constraint(equalTo: topAnchor),
+            profileHeaderImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            profileHeaderImageView.heightAnchor.constraint(equalToConstant: 180)
+        ]
+        
+        NSLayoutConstraint.activate(profileHeaderImageViewConstraints)
     }
 }
