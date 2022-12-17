3c520d9edce214aaf1dea0fd13ab66da71e8b49e
chavalk <chavalk@hotmail.com>
2022-12-17 12:18:00 -0600

    Add register view controller.

---
+++ TwitterClone/Controllers/Onboarding/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 12/17/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
