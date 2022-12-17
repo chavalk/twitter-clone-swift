b1c8bc04a15d0bcc44c7564821b7c73dcbe10c26
chavalk <chavalk@hotmail.com>
2022-12-17 12:19:48 -0600

    Add register title label.

--- TwitterClone/Controllers/Onboarding/RegisterViewController.swift
+++ TwitterClone/Controllers/Onboarding/RegisterViewController.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    private let registerTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Create your account"
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(registerTitleLabel)
     }
 }
