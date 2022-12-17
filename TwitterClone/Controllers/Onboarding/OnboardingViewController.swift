a2b14639f022696717ca9daa8ff833263f4852bf
chavalk <chavalk@hotmail.com>
2022-12-17 00:30:25 -0600

    Add welcome label and its constraints to onboarding view controller.

--- TwitterClone/Controllers/Onboarding/OnboardingViewController.swift
+++ TwitterClone/Controllers/Onboarding/OnboardingViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "See what's happening in the world right now."
+        label.font = .systemFont(ofSize: 32, weight: .heavy)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .label
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        view.addSubview(welcomeLabel)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        let welcomeLabelConstraints = [
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(welcomeLabelConstraints)
     }
 }
