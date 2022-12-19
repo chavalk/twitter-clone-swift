c69e44a5701dbc0e41f21c9bb0c560ca961c9343
chavalk <chavalk@hotmail.com>
2022-12-19 13:38:03 -0600

    Add auth manager with function to register user.

---
+++ TwitterClone/Networking/AuthManager.swift
@@ -0,0 +1,21 @@
+//
+//  AuthManager.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 12/19/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuthCombineSwift
+import Combine
+
+class AuthManager {
+    static let shared = AuthManager()
+    
+    func registerUser(with email: String, password: String) -> AnyPublisher<User, Error> {
+        return Auth.auth().createUser(withEmail: email, password: password)
+            .map(\.user)
+            .eraseToAnyPublisher()
+    }
+}
