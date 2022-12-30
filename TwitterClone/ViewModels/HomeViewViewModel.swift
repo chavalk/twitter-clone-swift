921a0a2a6cb778b75404b3c764ea4d4c60b67354
chavalk <chavalk@hotmail.com>
2022-12-30 16:31:10 -0600

    Complete retrieve user function to call upon database for twitter user.

--- TwitterClone/ViewModels/HomeViewViewModel.swift
+++ TwitterClone/ViewModels/HomeViewViewModel.swift
@@ -7,12 +7,23 @@
 
 import Foundation
 import Combine
+import FirebaseAuth
 
 final class HomeViewViewModel: ObservableObject {
     
     @Published var user: TwitterUser?
+    @Published var error: String?
     
     func retreiveUser() {
-        
+        guard let id = Auth.auth().currentUser?.uid else { return }
+        DatabaseManager.shared.collectionUsers(retrieve: id)
+            .sink { [weak self] completion in
+                if case .failure(let error) = completion {
+                    self?.error = error.localizedDescription
+                }
+            } receiveValue: { [weak self] user in
+                self?.user = user
+            }
+
     }
 }
