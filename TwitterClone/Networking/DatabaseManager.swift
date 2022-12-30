10fd6eec088a8537cfbe37bc4a3fc86bbb4f38fd
chavalk <chavalk@hotmail.com>
2022-12-29 18:19:24 -0600

    Add database manager.

---
+++ TwitterClone/Networking/DatabaseManager.swift
@@ -0,0 +1,29 @@
+//
+//  DatabaseManager.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 12/29/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+import FirebaseFirestoreCombineSwift
+import Combine
+
+class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    let db = Firestore.firestore()
+    let usersPath: String = "users"
+    
+    func collectionUsers(add user: User) -> AnyPublisher<Bool, Error> {
+        let twitterUser = TwitterUser(from: user)
+        return db.collection(usersPath).document(twitterUser.id).setData(from: twitterUser)
+            .map { _ in
+                return true
+            }
+            .eraseToAnyPublisher()
+    }
+}
