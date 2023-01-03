b0d3c62a330419a7f56fa0247455c42e1bfe8946
chavalk <chavalk@hotmail.com>
2023-01-03 02:54:08 -0600

    Modify profile avatar image view.

--- TwitterClone/Views/ProfileTableViewHeader.swift
+++ TwitterClone/Views/ProfileTableViewHeader.swift
@@ -148,14 +148,12 @@ class ProfileTableViewHeader: UIView {
         return label
     }()
     
-    private let profileAvatarImageView: UIImageView = {
+    var profileAvatarImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = true
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 40
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = UIImage(systemName: "person")
-        imageView.backgroundColor = .yellow
         return imageView
     }()
     
