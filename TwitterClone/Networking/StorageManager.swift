6e94d864b8d53dbcf5a4bdac6f31953e42da632a
chavalk <chavalk@hotmail.com>
2023-01-02 23:58:35 -0600

    Add get download URL function.

--- TwitterClone/Networking/StorageManager.swift
+++ TwitterClone/Networking/StorageManager.swift
@@ -10,13 +10,29 @@ import Combine
 import FirebaseStorageCombineSwift
 import FirebaseStorage
 
+enum FirestorageError: Error {
+    case invalidImageID
+}
+
 final class StorageManager {
     static let shared = StorageManager()
     
-    let storage = Storage.storage().reference()
+    let storage = Storage.storage()
+    
+    func getDownloadURL(for id: String?) -> AnyPublisher<URL, Error> {
+        guard let id = id else {
+            return Fail(error: FirestorageError.invalidImageID)
+                .eraseToAnyPublisher()
+        }
+        return storage
+            .reference(withPath: id)
+            .downloadURL()
+            .eraseToAnyPublisher()
+    }
     
     func uploadProfilePhoto(with randomID: String, image: Data, metaData: StorageMetadata) -> AnyPublisher<StorageMetadata, Error> {
         return storage
+            .reference()
             .child("images/\(randomID).jpg")
             .putData(image, metadata: metaData)
             .eraseToAnyPublisher()
