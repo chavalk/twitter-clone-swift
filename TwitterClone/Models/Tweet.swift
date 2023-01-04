fd8b5336da25b47920a23c458236be3bce59c833
chavalk <chavalk@hotmail.com>
2023-01-04 17:02:02 -0600

    Add tweet model.

---
+++ TwitterClone/Models/Tweet.swift
@@ -0,0 +1,18 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 1/4/23.
+//
+
+import Foundation
+
+struct Tweet: Codable {
+    let id: String
+    let author: TwitterUser
+    let tweetContent: String
+    var likesCount: Int
+    var likers: [String]
+    let isReply: Bool
+    let parentReference: String?
+}
