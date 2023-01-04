790761566b483a227f9be0f8dd34e972f78bd538
chavalk <chavalk@hotmail.com>
2023-01-04 00:31:03 -0600

    Add tweet compose view controller.

---
+++ TwitterClone/Controllers/General/TweetComposeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TweetComposeViewController.swift
+//  TwitterClone
+//
+//  Created by Jose Garcia on 1/4/23.
+//
+
+import UIKit
+
+class TweetComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
